46f5807022cbecc75e5722b8680dea089b90786a
Balaban Alexandru <balaban.alexandru94@gmail.com>
2018-03-21 15:32:53 +0200

    adding spring-boot

---
+++ src/main/java/tfb/springboot/raspberrypi/poc/RaspberryPocApplication.java
@@ -0,0 +1,12 @@
+package tfb.springboot.raspberrypi.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryPocApplication.class, args);
+	}
+}
---
+++ src/test/java/tfb/springboot/raspberrypi/poc/RaspberryPocApplicationTests.java
@@ -0,0 +1,16 @@
+package tfb.springboot.raspberrypi.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RaspberryPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
